9ce30eaf3c142dda3f036141747242b73894a3ce
Haloren <mckennakyler@gmail.com>
2020-10-08 00:09:58 -0700

    created seed file for starting cards

--- drawer-backend/db/seeds.rb
+++ drawer-backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Player.delete_all
+Player.create(name: 'Drawer')
+
+Card.delete_all
+words = [
+    'Hot Dog',
+    'Scream',
+    'Crane',
+    'Drop'
+]
+words.each do |word|
+    Card.create(content: word, player_id: Player.first.id)
+end
