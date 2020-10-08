b352c10745ed1057e697f833040b870a86321916
Haloren <mckennakyler@gmail.com>
2020-10-08 00:48:25 -0700

    started Players and Cards controllers for API

--- drawer-backend/app/controllers/cards_controller.rb
+++ drawer-backend/app/controllers/cards_controller.rb
@@ -1,2 +1,16 @@
 class CardsController < ApplicationController
+    def index
+        cards = Card.all
+        render json: cards
+    end
+
+    def show
+        card = Card.find_by(id: params[:id])
+        render json: card
+    end
+
+    def create
+
+    end
+
 end
--- drawer-backend/app/controllers/players_controller.rb
+++ drawer-backend/app/controllers/players_controller.rb
@@ -1,2 +1,18 @@
 class PlayersController < ApplicationController
+
+    def index
+        players = Player.all
+        render json: players, include: [:cards]
+    end
+
+    def show
+        player = Player.find_by(id: params[:id])
+        render json: player, include: [:cards]
+    end
+
+    def destroy
+        player = Player.all.find_by(id: params[:id])
+        player.destroy
+        render json: player, include: [:cards]
+    end
 end
