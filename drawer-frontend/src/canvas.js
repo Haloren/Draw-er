3b66082b82fe346372440ca410864a9cdf5580bd
Haloren <mckennakyler@gmail.com>
2020-10-09 20:19:39 -0700

    built a basic canvas to draw on

--- drawer-frontend/src/canvas.js
+++ drawer-frontend/src/canvas.js
@@ -2,4 +2,32 @@ window.addEventListener('load', () =>{
     // console.log("canvas")
     const canvas = document.querySelector("canvas");
     const context = canvas.getContext("2d");
+
+    canvas.height = window.innerHeight;
+    canvas.width = (window.innerWidth)/2;
+
+    let drawing = false;
+
+    function startDraw(e){
+        drawing = true;
+        draw(e);
+    }
+    function endDraw(){
+        drawing = false;
+        context.beginPath(); //restart a new seperate line
+    }
+    function draw(e){
+        if(!drawing) return;
+        context.lineWidth = 5; // THIS CHANGES THE LINE THICKNESS
+        context.strokeStyle = "blue"; // THIS CHANGES THE LINE COLOR
+        context.lineCap = "round";
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();        
+    }
+
+    canvas.addEventListener('mousedown', startDraw);
+    canvas.addEventListener('mouseup', endDraw);
+    canvas.addEventListener('mousemove', draw);
 })
+// MAKE A FUNCTION TO RESPONSIVELY CHANGE THE SIZE OF THE CANVAS
+// window.addEventListener('resize, ')
