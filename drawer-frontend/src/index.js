db62e1f8e87b8e678b6de1a8d0628bef56531785
Haloren <mckennakyler@gmail.com>
2020-10-11 23:18:43 -0700

    continued render all words and created delete button

--- drawer-frontend/src/index.js
+++ drawer-frontend/src/index.js
@@ -3,15 +3,53 @@ const CARDS_URL = `${BASE_URL}/cards`
 const PLAYERS_URL = `${BASE_URL}/players`
 
 const main = document.querySelector("main")
+const body = document.querySelector("body")
 
-//HELP & GAME RULES
-const showRules = () => {
-    let hide = document.getElementById("showRules");
-    if (hide.style.display === "none") {
-        hide.style.display = "flex";
-    } else {
-        hide.style.display = "none"
-    }
+//GET ALL CARDS
+document.addEventListener("DOMContentLoaded", () => loadCards())
+
+const loadCards = () => {
+    fetch(CARDS_URL)
+    .then(resp => resp.json())
+    .then(json => {
+        json.forEach(card => renderCards(card))
+    })
+}
+const renderCards = (cardList) => {
+    const div = document.createElement("div")
+    const ul = document.createElement("ul")
+    const button = document.createElement("button")
+    
+    div.setAttribute("class", "all-words hide")
+    div.setAttribute("id", cardList.id)
+
+    button.setAttribute("delete-word", cardList.id)
+    button.innerHTML = "Delete"
+    //add eventListener for button click
+
+    div.appendChild(ul)
+    div.appendChild(button)
+    body.appendChild(div)
+} 
+//SHOW ALL WORDS - HIDE/SHOW ON CLICK 
+const allWords = document.getElementById("all-words")
+
+//PLAYERS
+document.addEventListener("DOMContentLoaded", () => loadPlayers())
+
+const loadPlayers = () => {
+    fetch(PLAYERS_URL)
+    .then(resp => resp.json())
+    // .then(json => {
+    //     console.log(json)
+    // })
+    .then(json => {
+        json.forEach(player => renderPlayers(player))
+    })
+}
+const renderPlayers = (playerList) => {
+    const h3 = document.createElement("h3")
+
 }
 
 //GAME CONTROLS
@@ -31,59 +69,21 @@ function stopGame() {
 //GAME CARDS CONTAINER & TIMER (card-container)
 const cardsContainer = document.getElementById("cards-container")
 
-//GET ALL CARDS
-document.addEventListener("DOMContentLoaded", () => loadCards())
+//GET NEW WORDS FOR GAME CARDS
+//Math for random - cardList[Math.floor(Math.random() * cardList.length)];
+let randomCardOne = "Word One"
+let randomCardTwo = "Word Two"
+let cardOne = document.getElementById("card-one")
+cardOne.innerHTML = randomCardOne 
+let cardTwo = document.getElementById("card-two")
+cardTwo.innerHTML = randomCardTwo
 
-const loadCards = () => {
-    fetch(CARDS_URL)
-    .then(resp => resp.json())
-    .then(json => {
-        json.forEach(card => renderCards(card))
-    })
+//HELP & GAME RULES
+const showRules = () => {
+    let hide = document.getElementById("showRules");
+    if (hide.style.display === "none") {
+        hide.style.display = "flex";
+    } else {
+        hide.style.display = "none"
+    }
 }
-const renderCards = (cardList) => {
-    // console.log(cardList)
-    const ul = document.createElement("ul")
-    ul.setAttribute("class", "card-list hide")
-
-    const li = document.createElement("li")
-    li.setAttribute("data-id", cardList.id)
-    li.innerHTML = cardList.content
-
-    ul.appendChild(li)
-    main.appendChild(ul)
-    
-    //GET NEW WORDS FOR GAME CARDS
-    let randomCardOne = "Word One"
-    //cardList[Math.floor(Math.random() * cardList.length)];
-    let randomCardTwo = cardList.content
-
-    let cardOne = document.getElementById("card-one")
-    cardOne.innerHTML = randomCardOne 
-
-    let cardTwo = document.getElementById("card-two")
-    cardTwo.innerHTML = randomCardTwo
-}
-
-
-
-//SHOW ALL WORDS (ON CLICK) 
-const allWords = document.getElementById("all-words")
-
-//PLAYERS
-document.addEventListener("DOMContentLoaded", () => loadPlayers())
-
-const loadPlayers = () => {
-    fetch(PLAYERS_URL)
-    .then(resp => resp.json())
-    // .then(json => {
-    //     console.log(json)
-    // })
-    .then(json => {
-        json.forEach(player => renderPlayers(player))
-    })
-}
-const renderPlayers = (playerList) => {
-    console.log(playerList)
-}
-
